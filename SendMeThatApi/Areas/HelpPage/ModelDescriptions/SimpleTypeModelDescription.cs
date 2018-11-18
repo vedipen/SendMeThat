edc1d9c974f4b92d0d4a78956f10754297754d1a
Dipen Ved <dive@microsoft.com>
2018-11-18 15:11:53 +0530

    Add api project

---
+++ SendMeThatApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SendMeThatApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ SendMeThatApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SendMeThatApi.WebApiApplication" Language="C#" %>
