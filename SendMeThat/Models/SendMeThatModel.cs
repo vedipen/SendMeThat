701f28aa8164a76940e31cb5d0510daef0e692d4
Dipen Ved <dive@microsoft.com>
2018-11-24 14:14:55 +0530

    Complete sending

---
+++ SendMeThat/Models/SendMeThatModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendMeThat.Models
+{
+    public class SendMeThatModel
+    {
+        public string SendersEmail { get; set; }
+        public string ReceiversEmail { get; set; }
+        public System.DateTime SharedDate { get; set; }
+        public string SharedCode { get; set; }
+    }
+}
